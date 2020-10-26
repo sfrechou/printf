86f217e5c3ede3eb76d867a691bd1025d1d882de
Soledad Frechou <solefrechou@hotmail.com>
2020-10-26 15:16:26 +0000

    Add file fix

--- holberton.h
+++ holberton.h
@@ -11,7 +11,6 @@ int printchar(va_list list);
 int printstring(va_list list);
 int printpercent(va_list list);
 int printint(va_list list);
-int _printint(int bytes, int n);
 int _putchar(char c);
 int _strlen(char *s);
 int _printf(const char *format, ...);
--- print_functions.c
+++ print_functions.c
@@ -56,9 +56,8 @@ int printpercent(va_list arguments)
  */
 int printint(va_list arguments)
 {
-	int n, bytes;
+	int i, s, n, bytes = 0, cont = 0;
 
-	bytes = 0;
 	n = va_arg(arguments, int);
 	if (n < 0)
 	{
@@ -66,24 +65,18 @@ int printint(va_list arguments)
 		n *= -1;
 		bytes++;
 	}
-	bytes += _printint(bytes, n);
-	return (bytes);
-}
-
-/**
- * _printint - Recursive function to print an int.
- * @bytes: number of bytes.
- * @n: int value of argument.
- * Return: int.
- */
-int _printint(int bytes, int n)
-{
-	if ((n / 10) > 0)
+	s = n;
+	while ((n / 10) > 0)
 	{
-		_printint(bytes, (n / 10));
+		cont++;
+		n /= 10;
+	}
+	cont++;
+
+	for (i = 0; i < cont; i++, s /= 10)
+	{
+		_putchar((s % 10) + '0');
 		bytes++;
 	}
-	bytes++;
-	bytes += _putchar((n % 10) + '0');
 	return (bytes);
 }
